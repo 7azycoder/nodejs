d2d11aeba908278b4994cb78be2347c47131191e
Lovepreet Singh <dev.lovepreetsingh@gmail.com>
2016-05-04 23:37:21 +0530

    Added contact us and file uploading functionality plus csrf protection for contact us

--- index.js
+++ index.js
@@ -9,7 +9,12 @@ var handlebars = require('express-handlebars').create({defaultLayout:'main'});
 app.engine('handlebars',handlebars.engine);
 app.set('view engine', 'handlebars');
 
-//MORE IMPORST HERE
+app.use(require('body-parser').urlencoded({extended : true}));
+
+var formidable = require('formidable');
+
+var credentials = require('./credentials.js');
+app.use(require('cookie-parser')(credentials.cookieSecret));
 
 app.set('port', process.env.PORT || 3000);  //setting port for localhost
 
@@ -19,9 +24,7 @@ app.get('/', function(req, res){  // remeber path is case insensitive
 	res.render('home');   // it will render home.handlebars in  main.handlebars
 });
 
-app.get('/about', function(req, res){  // remeber path is case insensitive
-	res.render('about');   // it will render home.handlebars in  main.handlebars
-});
+
 
 // middleware
 app.use(function(req,res,next){                         
@@ -41,6 +44,45 @@ app.use(function(err,req,res,next){
 	next();
 });
 
+app.get('/about', function(req, res){  // remeber path is case insensitive
+	res.render('about');   // it will render home.handlebars in  main.handlebars
+});
+
+app.get('/contact', function(req,res){
+	res.render('contact',{csrf : 'CSRF token here'});
+});
+
+app.get('/thankyou', function(req,res){
+	res.render('thankyou');
+});
+
+app.post('/process', function(req,res){
+	console.log('Form : ' + req.query.form);
+	console.log('CSRF Token : ' + req.body._csrf);
+	console.log('Email : ' + req.body.email);
+	console.log('Question : '+ req.body.ques);
+	res.redirect(303, '/thankyou');
+});
+
+app.get('/file-upload', function(req, res ){
+	var now = new Date();
+	res.render('file-upload', {
+		year : now.getFullYear(),
+		month: now.getMonth()
+	});
+});
+
+app.post('/file-upload/:year/:month', function(req,res){
+	var form = new formidable.IncomingForm();
+	form.parse(req, function(err, fields, file){
+		if(err)
+			return res.redirect(303, '/error');
+		console.log("Received File");
+		console.log(file);
+		res.redirect(303,'/thankyou');
+	});
+});
+
 app.use(function(req,res){
 	res.type('text/html');
 	res.status(404);
@@ -51,7 +93,7 @@ app.use(function(err, req, res, next){
 	console.error(err.stack);
 	res.status(500);
 	res.render('500');
-})
+});
 
 
 
@@ -64,3 +106,4 @@ app.use(function(err, req, res, next){
 app.listen(app.get('port'), function(){
 	console.log("Express started on http:localhost:" + app.get('port') + 'Press Ctrl-C to terminate');
 });
+
