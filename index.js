72d46a2c24d32bb7cbc69c1122e8d5c72beaa1a4
Lovepreet Singh <dev.lovepreetsingh@gmail.com>
2016-05-04 22:07:55 +0530

    Added some middleware functions

--- index.js
+++ index.js
@@ -19,6 +19,42 @@ app.get('/', function(req, res){  // remeber path is case insensitive
 	res.render('home');   // it will render home.handlebars in  main.handlebars
 });
 
+app.get('/about', function(req, res){  // remeber path is case insensitive
+	res.render('about');   // it will render home.handlebars in  main.handlebars
+});
+
+// middleware
+app.use(function(req,res,next){                         
+	console.log("looking for URL : " + req.url);
+	next();
+});
+
+
+app.get('/junk',function(req,res, next){
+	console.log('Tried to access /junk');
+	throw new Error('/junk does not exit');
+});
+
+//middleware
+app.use(function(err,req,res,next){
+	console.log('Error : ' + err.message);
+	next();
+});
+
+app.use(function(req,res){
+	res.type('text/html');
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+})
+
+
+
 
 
 
